19496f1e5f19d6b0213281f328cdf00c504e07a6
Taewoong-H <taeung2008@gmail.com>
2021-08-16 21:29:11 +0900

    feat: webSocket으로 server와 front 연동해서 메시지 주고 받기 practice

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,21 @@
-alert('hi');
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+// server와 연결 했을 때
+socket.addEventListener('open', () => {
+  console.log('Connected to Server O');
+});
+
+// server에서 보낸 메시지를 받을 때
+socket.addEventListener('message', (message) => {
+  console.log('Just got this: ', message.data, 'from the Server');
+});
+
+// server와의 연결이 끊어졌을 때
+socket.addEventListener('close', () => {
+  console.log('Connected so Server X');
+});
+
+// server에 메시지를 보낼 때
+setTimeout(() => {
+  socket.send('hello from the browser!');
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
-import express from "express";
+import http from 'http';
+import WebSocket from 'ws';
+import express from 'express';
 
 const app = express();
 
@@ -6,7 +8,29 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => res.render('home'));
+app.get('/*', (req, res) => res.redirect('/'));
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
 
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+// http server create
+const server = http.createServer(app);
+// webSocket server create
+const wss = new WebSocket.Server({ server });
+// => 두개 서버 같이 같은 포트에 만듬
+
+wss.on('connection', (socket) => {
+  // browser에서 server에 접속할 때
+  console.log('Connected to Browser');
+  // browser connect가 종료되었을 때
+  socket.on('close', () => console.log('Disconnected from the Browser X'));
+  // browser에서 메시지를 보낼 때
+  socket.on('message', (message) => {
+    console.log(message.toString('utf8'));
+  });
+  // browser에 메시지를 보낼때
+  socket.send('hello!!!');
+});
+
+server.listen(3000, handleListen);
